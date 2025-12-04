2d98d0a2300197f92a1ee0eff8caa8ef03947149
Bharath <gl.bharath2006@gmail.com>
2025-12-04 11:15:22 +0530

    Js task 2

---
+++ JS/fun.js
@@ -0,0 +1,17 @@
+function add(x,y){
+    console.log("the sum of two number is",x+y);  
+}
+function sub(x,y){
+    console.log("the subraction of two number is",x-y);  
+}
+function mul(x,y){
+    console.log("the multiple of two number is",x*y);  
+}
+function div(x,y){
+    console.log("the division of two number is",x/y);  
+}
+
+add(5,5);
+sub(5,5);
+mul(5,5);
+div(5,5);
