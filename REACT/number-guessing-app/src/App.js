ee76b93f7f84518c7a8c36ed6aa9b6ac3edd244d
Bharath <gl.bharath2006@gmail.com>
2025-12-09 14:24:01 +0530

    Number Guessing Game in React

--- REACT/number-guessing-app/src/App.js
+++ REACT/number-guessing-app/src/App.js
@@ -1,27 +1,71 @@
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  let [number, setNumber] = useState("?");
+  let [guess,setGuess] = useState("");
+  let [score,setScore] = useState(20);
+  let [highScore,setHighScore] = useState(0);
+  let [msg,setMsg] = useState("Start Guessing ...");
+  let [bg,setBg] = useState("#222");
+ 
+  let [randomNumber,setRandomNumber] = useState(Math.trunc(Math.random() * 20) + 1);
+  // console.log(guess);
+  // console.log(randomNumber);
+ 
+  function handelCheck() {
+    let val = Number(guess);
+     if(!val){
+        setMsg("Enter the Number First ... ");
+    }
+    else if(val === randomNumber){
+        setNumber(randomNumber);
+        setBg("green");
+        setMsg("Correct Guess 🎉");
+        setHighScore(score > highScore ? score : highScore);
+    }
+    else if(val > randomNumber){
+        setMsg("Too High ...");
+        setScore(score - 1);
+    }
+    else if(val < randomNumber){
+        setMsg("Too Low ...");
+        setScore(score - 1);
+    }
+  }
+  function handleAgain() {
+    setHighScore(score > highScore ? score : highScore);
+    setScore(20);
+    setBg("#222");
+    setNumber('?');
+    setMsg("Start guessing...");
+    setRandomNumber(Math.trunc(Math.random() * 20) + 1);
+    setGuess("");
+  }
   return (
-    <div className="App">
-       <header>
+    <div className="App" style={{backgroundColor:bg}}>
+      <header>
         <h1>Guess My Number!</h1>
         <p class="between">(Between 1 and 20)</p>
-        <button class="btn again">Again!</button>
-        <div class="number">?</div>
-    </header>
-    <main>
+        <button class="btn again" onClick={handleAgain}>Again!</button>
+        <div class="number">{number}</div>
+      </header>
+      <main>
         <section class="left">
-            <input type="number" class="guess" />
-            <button class="btn check">Check!</button>
+          <input type="number" class="guess" value={guess} onChange={(e) => setGuess(e.target.value)}/>
+          <button class="btn check" onClick={handelCheck}>Check!</button>
         </section>
         <section class="right">
-            <p class="message">Start guessing...</p>
-            <p class="label-score">💯 Score: <span class="score">20</span></p>
-            <p class="label-highscore">
-                🥇 Highscore: <span class="highscore">0</span>
-            </p>
+          <p class="message">{msg}</p>
+          <p class="label-score">
+            💯 Score: <span class="score">{score}</span>
+          </p>
+          <p class="label-highscore">
+            🥇 Highscore: <span class="highscore">{highScore}</span>
+          </p>
         </section>
-    </main>
+      </main>
     </div>
   );
 }
