74ccf65a1d0fc08ee42ccb09163fa25458000753
Bharath <gl.bharath2006@gmail.com>
2025-12-09 12:19:22 +0530

    Number Guessing Game SetUp

--- REACT/counter-app/src/Counter.js
+++ REACT/counter-app/src/Counter.js
@@ -2,9 +2,18 @@ import "./App.css";
 import ButtonContainer from './ButtonContainer'
 
 const Counter = ({ val, handleIncrement, handleDecrement }) => {
+    let v = null;
+    if(val < 0){
+        v = <p>Negative</p>
+    }
+    else if(val > 1){
+        v = <p>Positive</p>
+    }
   return (
     <div className="counter">
       <h1>{val}</h1>
+      {/* {val < 0 ? <p>Negative</p> : null } */}
+      {v}
       <ButtonContainer handleIncrement = {handleIncrement} handleDecrement = {handleDecrement} />
     </div>
   );
