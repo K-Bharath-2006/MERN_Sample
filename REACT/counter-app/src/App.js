4e2469f500efb6c16bb21afcbb6db42d4946a1db
Bharath <gl.bharath2006@gmail.com>
2025-12-09 10:08:02 +0530

    Sending Js variable to HTML

--- REACT/counter-app/src/App.js
+++ REACT/counter-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Component1 from './Component1';
-import Com,{ Component2 } from './Component2';
+import Com,{ Component2 } from './Component2'; 
 import Com2,{Component3} from './Component3';
 function App() {
+  let msg = "Welcome Home Black";
+  let age = 20
   return (
     <div className="App">
       <h1>Welcome to React Learning</h1>
-      <Component1></Component1>
+      <Component1 q = {msg} val = {age}></Component1>
       <Component2></Component2>
       <Com></Com>
       <Component3></Component3>
--- REACT/counter-app/src/Component1.js
+++ REACT/counter-app/src/Component1.js
@@ -1,7 +1,9 @@
-const Component1 = () => {
+const Component1 = (props) => {
     return(
         <div>
             <h3>This is Component 1</h3>
+            <h4>{props.q}</h4>
+            <h4>{props.val}</h4>
         </div>
     );
 }
