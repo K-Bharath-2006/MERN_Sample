c88c6e35c507bf57b466a9ee8fd9b4065aa6e116
Bharath <gl.bharath2006@gmail.com>
2025-12-09 10:35:59 +0530

    nested components

--- REACT/counter-app/src/App.js
+++ REACT/counter-app/src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
-import Component1 from './Component1';
-import Com,{ Component2 } from './Component2'; 
-import Com2,{Component3} from './Component3';
+import GrandFather from './GrandFather';
 function App() {
   let msg = "Welcome Home Black";
-  let age = 20
+  // let age = 20
   return (
     <div className="App">
-      <h1>Welcome to React Learning</h1>
-      <Component1 q = {msg} val = {age}></Component1>
-      <Component2></Component2>
-      <Com></Com>
-      <Component3></Component3>
-      <Com2 ></Com2>
+      <h1>Welcome to Great Karikalan Magic Show</h1>
+      <GrandFather m = {msg} />
     </div>
   );
 }
